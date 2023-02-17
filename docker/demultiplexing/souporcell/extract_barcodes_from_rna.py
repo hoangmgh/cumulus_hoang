aee89ec97daa9062ab0e3289210938b5992b552c
hoangmgh <109824355+hoangmgh@users.noreply.github.com>
2023-02-17 14:46:58 -0500

    Update extract_barcodes_from_rna.py

--- docker/demultiplexing/souporcell/extract_barcodes_from_rna.py
+++ docker/demultiplexing/souporcell/extract_barcodes_from_rna.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
-
 from sys import argv, exit
 import pegasusio as io
-
+import scanpy as sc
 if len(argv) != 4:
-	print("Usage: python extract_barcodes_from_rna.py input_raw.h5 output_barcodes.tsv ngene")
+	print("Usage: python merge_data_across_batches.py input_sheet.csv output_barcodes.tsv ngene")
 	exit(-1)
+sheet=pg.read_csv(argv[2])
+for batch in sheet.batch:
+    
+    data = [sc.read_h5ad(h5) for h5 in sheet.RNA[sheet.batch==batch]]
+    [exec("data[i].obs_names=data[i].obs_names+'-'"+str(i)) for i in range(len(data))]
 
-data = io.read_input(argv[1])
-data.filter_data(min_genes=int(argv[3]))
+    [sc.pp.filter_cells(min_genes=int(argv[3])) for h5 in data]
+    data=sc.concat(data)
+    data=io.MultimodalData(data)
+    data.write_output(batch+"merged.h5")
 
-with open(argv[2], "w") as fout:
-	fout.write('\n'.join([x + '-1' for x in data.obs_names]) + '\n')
+    with open(batch+"output_barcodes.tsv", "w") as fout:
+        fout.write('\n'.join(data.obs_names) + '\n')
